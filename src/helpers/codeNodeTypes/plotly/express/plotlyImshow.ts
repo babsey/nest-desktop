09bc7ef8acdd4be7cbd1f1784b94519555e4651d
Sebastian Spreizer <spreizer@web.de>
2025-03-07 15:02:13 +0100

    add imshow to plotly code node type

---
+++ src/helpers/codeNodeTypes/plotly/express/plotlyImshow.ts
@@ -0,0 +1,40 @@
+// plotlyImshow.ts
+
+import { TextInputInterface } from "baklavajs";
+
+import { defineCodeNode } from "@/helpers/codeGraph/defineCodeNode";
+import { NodeInputInterface } from "@/helpers/codeGraph/interface/nodeInputInterface";
+import { NodeOutputInterface } from "@/helpers/codeGraph/interface/nodeOutputInterface";
+
+export default defineCodeNode({
+  type: "plotly.express.imshow",
+  title: "imshow",
+  inputs: {
+    data_frame: () => new NodeInputInterface("data_frame"),
+    x: () => new TextInputInterface("x", ""),
+    y: () => new TextInputInterface("y", ""),
+  },
+  outputs: {
+    out: () => new NodeOutputInterface(),
+  },
+  codeTemplate() {
+    if (!this.node) return this.type;
+    const args = [];
+
+    const data_frame = this.node.getConnectedNodesByInterface("data_frame");
+    if (data_frame.length > 0) args.push(`${this.code?.graph.formatLabels(data_frame).join(", ")}`);
+
+    const x = this.node.getConnectedOutputInterfaceByInterface("x");
+    if (x.length > 1) args.push(`x=[${this.code?.graph.formatInterfaceLabels(x).join(", ")}]`);
+    else if (x.length > 0) args.push(`x=${this.code?.graph.formatInterfaceLabels(x).join(", ")}`);
+    else if (this.node.inputs.x.value) args.push(`x="${this.node.inputs.x.value}"`);
+
+    const y = this.node.getConnectedOutputInterfaceByInterface("y");
+    if (y.length > 1) args.push(`y=[${this.code?.graph.formatInterfaceLabels(y).join(", ")}]`);
+    else if (y.length > 0) args.push(`y=${this.code?.graph.formatInterfaceLabels(y).join(", ")}`);
+    else if (this.node.inputs.y.value) args.push(`y="${this.node.inputs.y.value}"`);
+
+    return `px.imshow(${args.join(", ")})`;
+  },
+  variableName: "fig",
+});
--- src/helpers/codeNodeTypes/plotly/index.ts
+++ src/helpers/codeNodeTypes/plotly/index.ts
@@ -3,16 +3,19 @@
 import { useCodeGraphStore } from "@/stores/graph/codeGraphStore";
 
 // import plotlyGraphObjectsBar from "./graph_objects/plotlyBar";
-import plotlyDataResponse from "./plotlyDataResponse";
-// import plotlyGraphObjectsFigure from "./graph_objects/plotlyFigure";
-// import plotlyFigureAddTrace from "./plotlyFigureAddTrace";
-// import plotlyGraphObjectsHeatmap from "./graph_objects/plotlyHeatmap";
-// import plotlyMakeSubplots from "./plotlyMakeSubplots";
-// import plotlyGraphObjectsScatter from "./graph_objects/plotlyScatter";
-import plotlyScatter from "./express/plotlyScatter";
-import plotlyLine from "./express/plotlyLine";
-import plotlyHistogram from "./express/plotlyHistogram";
 import plotlyBar from "./express/plotlyBar";
+import plotlyDataResponse from "./plotlyDataResponse";
+// import plotlyFigureAddTrace from "./plotlyFigureAddTrace";
+// import plotlyGraphObjectsBar from "./graph_objects/plotlyBar";
+// import plotlyGraphObjectsFigure from "./graph_objects/plotlyFigure";
+// import plotlyGraphObjectsHeatmap from "./graph_objects/plotlyHeatmap";
+// import plotlyGraphObjectsScatter from "./graph_objects/plotlyScatter";
+// import plotlyGraphObjectsScattergl from "./graph_objects/plotlyScattergl";
+import plotlyHistogram from "./express/plotlyHistogram";
+import plotlyImshow from "./express/plotlyImshow";
+import plotlyLine from "./express/plotlyLine";
+// import plotlyMakeSubplots from "./plotlyMakeSubplots";
+import plotlyScatter from "./express/plotlyScatter";
 
 export const registerPlotlyNodeTypes = () => {
   const codeGraphStore = useCodeGraphStore();
@@ -29,8 +32,9 @@ export const registerPlotlyNodeTypes = () => {
   // editor.registerNodeType(plotlyGraphObjectsFigure, { category: "plotly.graph_objects" });
   // editor.registerNodeType(plotlyGraphObjectsHeatmap, { category: "plotly.graph_objects" });
   // editor.registerNodeType(plotlyGraphObjectsScatter, { category: "plotly.graph_objects" });
-  // editor.registerNodeType(plotlyGraphObjectsScatter, { category: "plotly.graph_objects" });
+  // editor.registerNodeType(plotlyGraphObjectsScattergl, { category: "plotly.graph_objects" });
   editor.registerNodeType(plotlyHistogram, { category: "plotly.express" });
+  editor.registerNodeType(plotlyImshow, { category: "plotly.express" });
   editor.registerNodeType(plotlyLine, { category: "plotly.express" });
   // editor.registerNodeType(plotlyMakeSubplots, { category: "plotly.subplots" });
   editor.registerNodeType(plotlyScatter, { category: "plotly.express" });
